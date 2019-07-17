ffb45b81cd7c615df35fb932bcc2390a7a18ef80
Paul Pan <panjunhong05@gmail.com>
2019-07-16 20:04:22 -0700

    Added rendering of various components of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import TopBar from './components/TopBar'
+import AboutMe from './components/AboutMe'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Coursework from './components/Coursework'
 
-class App extends React.Component {
-  render() {
-    return (<TopBar />);
-  }
+const App: React.FC = () => {
+  const aboutMeRef = React.useRef(null);
+  const experienceRef = React.useRef(null);
+  const projectsRef = React.useRef(null);
+  const courseworkRef = React.useRef(null);
+  return (
+    <React.Fragment>
+      <TopBar
+        aboutMe={aboutMeRef} experience={experienceRef}
+        projects={projectsRef} coursework={courseworkRef}
+      />
+      <div ref={aboutMeRef}>
+        <AboutMe />
+      </div>
+      <div ref={experienceRef}>
+        <Experience />
+      </div>
+      <div ref={projectsRef}>
+        <Projects />
+      </div>
+      <div ref={courseworkRef}>
+        <Coursework />
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
