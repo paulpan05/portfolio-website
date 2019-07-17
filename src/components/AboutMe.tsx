3aa167d5ea1d3cfbed34d2aea63b64186c769c8d
Paul Pan <panjunhong05@gmail.com>
2019-07-16 21:23:20 -0700

    Basic about me layout

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar'
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import profile from '../images/profile.jpg'
+
+const useStyles = makeStyles(
+  createStyles({
+    avatar: {
+      margin: 10,
+      width: 200,
+      height: 200
+    }
+  })
+);
 
 const AboutMe: React.FC = () => {
-  return(<div></div>);
+  const classes = useStyles();
+  return(<Avatar alt="Remy Sharp" src={profile} className={classes.avatar} />);
 }
 
 export default AboutMe;
