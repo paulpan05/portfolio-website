c0254f322e7bcf8c0a99ed7fc5431515a09ac02f
Paul Pan <panjunhong05@gmail.com>
2019-07-17 20:29:33 -0700

    Changed images and added styles

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar'
+import Avatar from '@material-ui/core/Avatar';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import profile from '../images/profile.jpg'
+import profile from '../images/profile.jpg';
+import background from '../images/background.jpg';
 
 const useStyles = makeStyles(
   createStyles({
     avatar: {
-      margin: 10,
       width: 200,
       height: 200
+    },
+    masthead: {
+      height: '100vh',
+      minHeight: '500px',
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat'
     }
   })
 );
 
 const AboutMe: React.FC = () => {
   const classes = useStyles();
-  return(<Avatar alt="Remy Sharp" src={profile} className={classes.avatar} />);
+  return(
+    <header className={classes.masthead}>
+      <Avatar alt="Remy Sharp" src={profile} className={classes.avatar} />
+    </header>
+  );
 }
 
 export default AboutMe;
