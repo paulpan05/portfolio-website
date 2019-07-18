c8db9650c00968ec899bae52d8a01d79bcdebd50
Paul Pan <panjunhong05@gmail.com>
2019-07-17 21:49:10 -0700

    About me page style modified to center content

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,20 +3,25 @@ import Avatar from '@material-ui/core/Avatar';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import profile from '../images/profile.jpg';
 import background from '../images/background.jpg';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(
   createStyles({
     avatar: {
-      width: 200,
-      height: 200
+      width: 250,
+      height: 250
     },
     masthead: {
+      background: `url(${background}) no-repeat center center fixed`,
+      WebkitBackgroundSize: "cover",
+      MozBackgroundSize: "cover",
+      backgroundSize: "cover",
+      OBackgroundSize: "cover"
+    },
+    grid: {
       height: '100vh',
       minHeight: '500px',
-      backgroundImage: `url(${background})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat'
+      verticalAlign: 'middle'
     }
   })
 );
@@ -25,7 +30,15 @@ const AboutMe: React.FC = () => {
   const classes = useStyles();
   return(
     <header className={classes.masthead}>
-      <Avatar alt="Remy Sharp" src={profile} className={classes.avatar} />
+      <Grid
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.grid}
+      >
+        <Avatar alt="Remy Sharp" src={profile} className={classes.avatar} />
+      </Grid>
     </header>
   );
 }
