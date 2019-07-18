fbc559963add25b50029b6fec6da4fce34bb824c
Paul Pan <panjunhong05@gmail.com>
2019-07-18 00:26:16 -0700

    Handled bar transparency based on scrolling position

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -3,10 +3,15 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
 import page from '../constants/page'
 
 const scrollToComponent = (ref: React.RefObject<any>) => {
   window.scrollTo(0, ref.current.offsetTop);
+}
+
+const scrolledUp = (ref: React.RefObject<any>): boolean => {
+  return window.pageYOffset < ref.current.offsetTop - 48;
 }
 
 type TopBarProps = {
@@ -14,9 +19,26 @@ type TopBarProps = {
   experience: React.RefObject<any>
   projects: React.RefObject<any>
   coursework: React.RefObject<any>
+  profile: React.RefObject<any>
 }
 
-const TopBar: React.FC<TopBarProps> = (props: React.PropsWithChildren<TopBarProps>) => {
+const useStyles = makeStyles(
+  createStyles({
+    appBarTransparent: {
+      backgroundColor: 'transparent',
+      boxShadow: 'none',
+      WebkitBoxShadow: 'none',
+      MozBoxShadow: 'none'
+    },
+    tabs: {
+      height: '48'
+    }
+  })
+);
+
+const TopBar: React.FC<TopBarProps> = (props) => {
+  const classes = useStyles();
+  const [barClass, setBarClass] = React.useState<string | undefined>(classes.appBarTransparent);
   const tabChange = (_event: React.ChangeEvent<{}>, value: number): void => {
     switch (value) {
       case page.AboutMe:
@@ -34,9 +56,12 @@ const TopBar: React.FC<TopBarProps> = (props: React.PropsWithChildren<TopBarProp
       default:
     }
   }
+  window.onscroll = () => {
+    scrolledUp(props.profile) ? setBarClass(classes.appBarTransparent) : setBarClass(undefined);
+  }
   const smallScreen: boolean = useMediaQuery('(min-width: 600px)');
   return (
-    <AppBar>
+    <AppBar className={barClass}>
       <Tabs
         value={false}
         onChange={tabChange}
@@ -44,7 +69,7 @@ const TopBar: React.FC<TopBarProps> = (props: React.PropsWithChildren<TopBarProp
         variant={smallScreen ? undefined : "scrollable"}
         scrollButtons={smallScreen ? undefined : "on"}
       >
-        <Tab label="About me" />
+        <Tab label="About me" className={classes.tabs} />
         <Tab label="Experience" />
         <Tab label="Projects" />
         <Tab label="Coursework" />
