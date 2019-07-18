3560a00995a162930c841c8311845676b42e0e69
Paul Pan <panjunhong05@gmail.com>
2019-07-17 20:28:47 -0700

    Included css file in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import 'typeface-roboto';
 import 'material-design-icons';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/index.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
