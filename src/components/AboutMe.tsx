0a5eae0a70f727336b9042afba564b2a82bebccc
Paul Pan <panjunhong05@gmail.com>
2019-07-20 15:29:56 -0700

    Included styling in AboutMe component

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles, createStyles } from '@material-ui/core/styles';
 import profile from '../images/profile.jpg';
+import '../css/AboutMe.css'
 
 const useStyles = makeStyles(
   createStyles({
@@ -25,10 +27,67 @@ const useStyles = makeStyles(
 
 type AboutMeProps = {
   profileRef: React.RefObject<HTMLDivElement>
-}
+};
 
 const AboutMe: React.FC<AboutMeProps> = (props) => {
   const classes = useStyles();
+  const transitionComponents = [
+    {
+      in: true, 
+      appear: true, 
+      timeout: 1000, 
+      classNames: "avatar",
+      element: 
+        <Avatar
+          alt="Remy Sharp" 
+          src={profile}
+          className={classes.avatar}
+          ref={props.profileRef}
+        />
+    },
+    {
+      in: true, 
+      appear: true, 
+      timeout: 1500, 
+      classNames: "self-intro",
+      element:
+      <Typography
+        variant="h2"
+        className={classes.headerText}
+        gutterBottom
+      >
+        This is Paul
+      </Typography> 
+    },
+    {
+      in: true, 
+      appear: true, 
+      timeout: 2000, 
+      classNames: "self-descr",
+      element:
+        <Typography
+          variant="h4"
+          className={classes.headerText}
+          gutterBottom
+        >
+          I develop full-stack web and mobile applications.
+        </Typography>
+    },
+    {
+      in: true, 
+      appear: true, 
+      timeout: 2500, 
+      classNames: "self-edu",
+      element:
+        <Typography
+          variant="h6"
+          className={classes.headerText}
+          gutterBottom
+        >
+          B.S. Computer Science, UC San Diego, Graduating June 2022
+        </Typography>
+    }
+  ];
   return (
     <Grid
       container
@@ -37,16 +96,16 @@ const AboutMe: React.FC<AboutMeProps> = (props) => {
       alignItems="center"
       className={classes.grid}
     >
-      <Avatar alt="Remy Sharp" src={profile} className={classes.avatar} ref={props.profileRef} />
-      <Typography variant="h2" className={classes.headerText} gutterBottom>
-        This is Paul
-      </Typography>
-      <Typography variant="h4" className={classes.headerText} gutterBottom>
-        I develop full-stack web and mobile applications.
-      </Typography>
-      <Typography variant="h6" className={classes.headerText} gutterBottom>
-        B.S. Computer Science, UC San Diego, Graduating June 2022
-      </Typography>
+      { transitionComponents.map((component) => 
+        <CSSTransition
+          in={component.in}
+          appear={component.appear}
+          timeout={component.timeout}
+          classNames={component.classNames}
+        >
+          {component.element}
+        </CSSTransition>
+      )}
     </Grid>
   );
 }
